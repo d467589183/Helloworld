079d7d11c66df357ecc163ca6158e0aba28ec47d
Duan <Duan@PC>
2013-04-20 14:48:25 +0800

    master update 1th

--- src/github/Main.java
+++ src/github/Main.java
@@ -3,7 +3,7 @@ package github;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		//I have merged
+		//I have merged 1th
 		System.out.println("Untrack");
 	}
 }
