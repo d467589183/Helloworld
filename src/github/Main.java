9694f0f0265955557d8dcdd1e2ff17977a05ac4f
Duan <Duan@PC>
2013-04-21 22:19:44 +0800

    update Main.java

--- src/github/Main.java
+++ src/github/Main.java
@@ -2,9 +2,10 @@ package github;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		//I have merged 2th
-		//Untrack
-		System.out.println("Untrack");
+//		System.out.println("HelloWorld");
+//		//I have merged 2th
+//		//Untrack
+//		System.out.println("Untrack");
+		System.out.println("add navtive.java");
 	}
 }
